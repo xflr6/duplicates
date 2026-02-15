1a09bc43397ac419080c7b451aedc31200c83f32
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2026-02-15 11:02:26 +0100

    add `requires-python` and `dependencies` as PEP 723 inline script metadata - https://packaging.python.org/en/latest/specifications/inline-script-metadata/ - https://peps.python.org/pep-0723 - https://hatch.pypa.io/latest/how-to/run/python-scripts/

--- duplicates.py
+++ duplicates.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.11"
+# dependencies = ["sqlalchemy>=1.4"]
+# ///
 
 """Create duplicate file CSV report via SQLite database with file infos."""
 
